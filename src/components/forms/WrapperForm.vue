bd5ab44b93fb0bf47b6c39b771d7b42cc5ccb170
Lucas Araripe <pedro.lucx@gmail.com>
2024-02-02 06:39:16 -0300

    feature : initializing project

---
+++ src/components/forms/WrapperForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="wrapper-form">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper-form {
+    background-color: var(--main-color-form);
+    padding: 2rem;
+    width: max(30%, 500px);
+}
+</style>
---
+++ src/views/PasswordGenerationView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapper-password-generation">
+        <h1>
+            Password Generator
+        </h1>
+
+        <WrapperForm>
+            Test
+        </WrapperForm>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import WrapperForm from "@/components/forms/WrapperForm.vue";
+</script>
+
+<style lang="scss" scoped>
+.wrapper-password-generation {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & > h1 {
+        margin-bottom: 2rem;
+        color: var(--light-main-color-form);
+        font-weight: 500;
+        font-size: 1.5rem;
+        letter-spacing: 0.1rem;
+    }
+}
+</style>
