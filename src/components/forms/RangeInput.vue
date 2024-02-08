bd33beee0311da0fd6c6aaf644c1ca591a1a0d1a
Lucas Araripe <pedro.lucx@gmail.com>
2024-02-08 06:48:26 -0300

    style : fixing spacement bottom in range input

--- src/components/forms/RangeInput.vue
+++ src/components/forms/RangeInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper-range-input" :style="`--current-range: ${currentPercentageRange}`">
-    <div class="labels row align-items-center">
-      <label :for="label" class="col-11 form-base-text-color">
+    <div class="labels row align-items-center mb-2">
+      <label :for="label" class="col-11">
         {{ label }}
       </label>
       
@@ -58,8 +58,6 @@ const currentPercentageRange = computed(() => {
   --current-range: 0%;
 
   & .labels {
-    margin-bottom: 1.5rem;
-
     label {
       font-size: 1.1rem;
     }
